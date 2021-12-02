412d216a99d0c28a4253292901600ce86c3862e5
unknown <kvnftzsmmns@gmail.com>
2021-12-01 17:18:07 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double value1 = 14.99;
+		double value2 = 15.99;
+		
+		System.out.println(value1+value2);
+	}
+}
